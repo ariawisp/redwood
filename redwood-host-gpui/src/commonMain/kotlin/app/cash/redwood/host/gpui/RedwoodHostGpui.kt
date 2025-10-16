5c2d74a24fb2c111e41b5fd1fc5f316dddfd421e
Aria Wisp <aria@ariawisp.com>
2025-10-15 23:48:43 -0600

    gpui initial

---
+++ redwood-host-gpui/src/commonMain/kotlin/app/cash/redwood/host/gpui/RedwoodHostGpui.kt
@@ -0,0 +1,7 @@
+package app.cash.redwood.host.gpui
+
+/**
+ * Placeholder type to keep the module producing Kotlin metadata
+ * while the UniFFI bindings are being integrated.
+ */
+public object RedwoodHostGpui
