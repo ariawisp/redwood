6979cacff23d2d814b799c5014b901d71a4f8156
Aria Wisp <aria@ariawisp.com>
2025-10-16 06:36:33 -0600

    more

--- samples/gpui-host/src/macosArm64Main/kotlin/app/cash/redwood/samples/gpui/Main.kt
+++ samples/gpui-host/src/macosArm64Main/kotlin/app/cash/redwood/samples/gpui/Main.kt
@@ -1,13 +1,28 @@
+/*
+ * Copyright (C) 2025 Square, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package app.cash.redwood.samples.gpui
 
+import app.cash.redwood.host.gpui.GpuiApp
 import app.cash.redwood.host.gpui.GpuiAppDelegate
 import app.cash.redwood.host.gpui.GpuiAppOptions
-import app.cash.redwood.host.gpui.GpuiApp
+import app.cash.redwood.host.gpui.GpuiRedwoodView
 import app.cash.redwood.host.gpui.GpuiWindowConfig
 import app.cash.redwood.host.gpui.GpuiWindowSize
-import app.cash.redwood.host.gpui.runGpuiApp
-import app.cash.redwood.host.gpui.GpuiRedwoodView
 import app.cash.redwood.host.gpui.createRedwoodView
+import app.cash.redwood.host.gpui.runGpuiApp
 import app.cash.redwood.layout.api.Constraint
 import app.cash.redwood.layout.gpui.GpuiRedwoodLayoutWidgetFactory
 import app.cash.redwood.ui.Density
